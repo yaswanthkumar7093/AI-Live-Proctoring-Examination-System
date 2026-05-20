519d1b2d46594e3b68c8687e07f4225dc45035bb
User <user@example.com>
2026-05-20 10:16:28 +0530

    Force vercel builds config to fix deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../public',
-    emptyOutDir: true
-  },
   server: {
     proxy: {
       '/api': {
